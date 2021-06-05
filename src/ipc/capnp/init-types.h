b95d9d593347bee2f26281cca320352771a8d592
ChymeraCoin <85292847+ChymeraCoin@users.noreply.github.com>
2021-06-06 01:56:19 +0800

    Add files via upload

---
+++ src/ipc/capnp/init-types.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 The chymera Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef chymera_IPC_CAPNP_INIT_TYPES_H
+#define chymera_IPC_CAPNP_INIT_TYPES_H
+
+#include <ipc/capnp/echo.capnp.proxy-types.h>
+
+#endif // chymera_IPC_CAPNP_INIT_TYPES_H
