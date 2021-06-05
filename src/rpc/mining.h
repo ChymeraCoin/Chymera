e669ebd59ab4da9d65ffe297b5f1cc0c7b639505
ChymeraCoin <85292847+ChymeraCoin@users.noreply.github.com>
2021-06-06 02:51:11 +0800

    Add files via upload

---
+++ src/rpc/mining.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 The chymera Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef chymera_RPC_MINING_H
+#define chymera_RPC_MINING_H
+
+/** Default max iterations to try in RPC generatetodescriptor, generatetoaddress, and generateblock. */
+static const uint64_t DEFAULT_MAX_TRIES{1000000};
+
+#endif // chymera_RPC_MINING_H
