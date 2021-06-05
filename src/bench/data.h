c6b53fd456e42aab67100e6d49363b70734699db
ChymeraCoin <85292847+ChymeraCoin@users.noreply.github.com>
2021-06-06 01:20:42 +0800

    Add files via upload

---
+++ src/bench/data.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 The chymera Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef chymera_BENCH_DATA_H
+#define chymera_BENCH_DATA_H
+
+#include <cstdint>
+#include <vector>
+
+namespace benchmark {
+namespace data {
+
+extern const std::vector<uint8_t> block413567;
+
+} // namespace data
+} // namespace benchmark
+
+#endif // chymera_BENCH_DATA_H
