257bc6602c3a0b512c0ce0ecbc45e1ace8ab453b
ChymeraCoin <85292847+ChymeraCoin@users.noreply.github.com>
2021-06-06 03:19:00 +0800

    Add files via upload

---
+++ src/zmq/zmqrpc.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 The chymera Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef chymera_ZMQ_ZMQRPC_H
+#define chymera_ZMQ_ZMQRPC_H
+
+class CRPCTable;
+
+void RegisterZMQRPCCommands(CRPCTable& t);
+
+#endif // chymera_ZMQ_ZMRRPC_H
---
+++ src/zmq/zmqutil.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2014-2018 The chymera Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef chymera_ZMQ_ZMQUTIL_H
+#define chymera_ZMQ_ZMQUTIL_H
+
+void zmqError(const char* str);
+
+#endif // chymera_ZMQ_ZMQUTIL_H
